d3ced342c7b0fc024153d78a4c147b68778efc58
oscarsil <osgisilo@gmail.com>
2022-01-05 15:03:26 -0700

    model de wireguard

---
+++ database/factories/WireguardFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class WireguardFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
+
+
+/**$faker->unique()-
+ * $faker->ipv4();
+ */
---
+++ database/migrations/2022_01_04_234608_create_wireguards_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWireguardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wireguards', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('ready')->default(0)->comment('Wether the wireguard has been set up.');
+            $table->string('generated_public_key')->nullable();
+            $table->unsignedBigInteger('customer_id')->nullable();
+            $table->timestamps();
+            $table->foreign('customer_id')->references('id')->on('customers')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wireguards');
+    }
+}
