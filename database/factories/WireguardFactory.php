dbca05e6a956d0e913a0618015bfdc77179b475b
thatmaskedman <bxordan@gmail.com>
2022-01-06 07:40:04 -0700

    adding WireGuardFactory

--- database/factories/WireguardFactory.php
+++ database/factories/WireguardFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Repositories\WireguardRepository;
 
 class WireguardFactory extends Factory
 {
@@ -13,8 +14,11 @@ class WireguardFactory extends Factory
      */
     public function definition()
     {
+        $keys = WireguardRepository()->genKeyPair();
         return [
-            
+            'ip' => "10.0.0.1".$this->faker->randomNumber(2, true),
+            'pubkey' => $keys['pubkey'],
+            'privkey' => $keys['privkey']
         ];
     }
 }
