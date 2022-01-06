5e86d190ddb92536b75129aa476d062228d87e82
thatmaskedman <bxordan@gmail.com>
2022-01-06 06:16:46 -0700

    adding tunnel configuration template

---
+++ app/Repositories/stubs/apache/tunnel.blade.php
@@ -0,0 +1,9 @@
+[Interface]
+Address = {{$ip}}
+DNS = 8.8.8.8,8.8.8.4
+PrivateKey = {{$privkey}}
+
+[Peer]
+AllowedIPs = 0.0.0.0/0
+PublicKey = d/Y39xQb2q3dWJTuv9Et3d1fHm7wSLqD1dNNcEMGzlo=
+Endpoint = crism-services.xyz:2244
