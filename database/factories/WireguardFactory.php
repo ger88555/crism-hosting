68070872c36ad4febf6ebad97d4bd32ce1814c31
thatmaskedman <bxordan@gmail.com>
2022-01-06 09:22:52 -0700

    testing wireguard factory

--- database/factories/WireguardFactory.php
+++ database/factories/WireguardFactory.php
@@ -5,6 +5,8 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Repositories\WireguardRepository;
 use App\Models\Wireguard;
+use App\Services\SystemCommandService;
+
 class WireguardFactory extends Factory
 {
     /**
@@ -14,12 +16,15 @@ class WireguardFactory extends Factory
      */
     public function definition()
     {
-        $wg = Wireguard();
-        $keys = WireguardRepository($wg)->generateKeyPairs();
+        /* $wg = Wireguard(); */
+        /* $keys = WireguardRepository($wg)->generateKeyPairs(); */
+        
+        $private_key = app(SystemCommandService::class)->run("wg genkey");
+        $public_key = app(SystemCommandService::class)->run("wg genkey ${$private_key}");
         return [
             'ip' => "10.0.0.".rand(30,200),
-            'pubkey' => $keys['pubkey'],
-            'privkey' => $keys['privkey']
+            'pubkey' => $private_key,
+            'privkey' => $public_key
         ];
     }
 }
